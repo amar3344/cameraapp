990d92d34513abf8f1515a4e29714529a1dd2075
janaki.telugu <janaki.telugu@extwebtech.in>
2023-06-28 19:30:20 +0530

    donr

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -1,20 +1,69 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, View, Alert } from 'react-native'
+import { RNCamera } from 'react-native-camera'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import Icon from 'react-native-vector-icons/dist/FontAwesome'
 
 interface IProps{
-    navigation:{
-        navigate:(arg:string)=>void
+
+}
+interface IState{
+    takingPic:boolean
+}
+
+class Camera extends Component<IProps,IState> {
+
+    state:IState = {takingPic:false}
+    camera?: RNCamera | null
+
+    takePicture = async () => {
+        if (this.camera && !this.state.takingPic) {
+    
+          let options = {
+            quality: 0.85,
+            fixOrientation: true,
+            forceUpOrientation: true,
+          };
+    
+          this.setState({takingPic: true});
+    
+          try {
+             const data = await this.camera.takePictureAsync(options);
+             console.log('Success', JSON.stringify(data));
+          } catch (err:any) {
+            Alert.alert('Error', 'Failed to take picture: ' + (err.message || err));
+            return;
+          } finally {
+            this.setState({takingPic: false});
+          }
+        }
+      };
+
+    render() {
+      return (
+        <View style={styles.container}>
+          <RNCamera captureAudio={false}
+            style={{ flex: 1, alignItems: 'center' }}
+            ref={ref => {
+              this.camera = ref
+            }}
+          />
+          <TouchableOpacity style={{justifyContent:'center',alignItems:'center'}} onPress={this.takePicture}>
+            <Icon name="camera" size={50}color={"#fff"}></Icon>
+          </TouchableOpacity>
+          
+        </View>
+      )
     }
-}
-
-export class Camera extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
   }
-}
 
-export default Camera
+  export default Camera
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: 'black'
+    }
+  })
+  
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Text, View ,PermissionsAndroid,Platform, Alert} from 'react-native'
-import { RNCamera } from 'react-native-camera'
+import { Text, View ,PermissionsAndroid, Alert} from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import {check, RESULTS,PERMISSIONS } from 'react-native-permissions';
 
 interface IProps{
     navigation?:{
@@ -18,21 +18,29 @@ export class Home extends Component<IProps> {
     }
 
     handleCameraScreen=async()=>{
-         if(Platform.OS === "android"){
-            const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.CAMERA,{
-                    title:'Example App Camera Permission',
-                    message:'Example App needs access to your camera'
-                },
-            );
-            if(granted === PermissionsAndroid.RESULTS.granted){
+        check(PermissionsAndroid.PERMISSIONS.CAMERA)
+        .then((result) => {
+          switch (result) {
+            case RESULTS.UNAVAILABLE:
+              Alert.alert('This feature is not available (on this device / in this context)');
+              break;
+            case RESULTS.DENIED:
+              Alert.alert('The permission has not been requested / is denied but requestable');
+              break;
+            case RESULTS.LIMITED:
                 this.proceed()
-            }else{
-                Alert.alert('Camera Permissions are Denied')
-            }
-         }else{
-            this.proceed()
-         }
+                break;
+            case RESULTS.GRANTED:
+                this.proceed()
+                break;
+            case RESULTS.BLOCKED:
+              Alert.alert('The permission is denied and not requestable anymore');
+              break;
+          }
+        })
+        .catch(() => {
+          console.log(Error)
+        });
     }
 
     render() {
@@ -41,10 +49,33 @@ export class Home extends Component<IProps> {
             <TouchableOpacity onPress={this.handleCameraScreen}>
                 <Text>Go to Camerea</Text>
             </TouchableOpacity>
-            <RNCamera captureAudio={false}/>
         </View>
     )
   }
 }
 
 export default Home
+
+// check(PERMISSIONS.IOS.LOCATION_ALWAYS)
+//   .then((result) => {
+//     switch (result) {
+//       case RESULTS.UNAVAILABLE:
+//         console.log('This feature is not available (on this device / in this context)');
+//         break;
+//       case RESULTS.DENIED:
+//         console.log('The permission has not been requested / is denied but requestable');
+//         break;
+//       case RESULTS.LIMITED:
+//         console.log('The permission is limited: some actions are possible');
+//         break;
+//       case RESULTS.GRANTED:
+//         console.log('The permission is granted');
+//         break;
+//       case RESULTS.BLOCKED:
+//         console.log('The permission is denied and not requestable anymore');
+//         break;
+//     }
+//   })
+//   .catch((error) => {
+//     // …
+//   });
